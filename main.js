27f8aa57840b3c79c3bff25fc50c469026a9bd6c
hdiiofficial <79190751+hdiiofficial@users.noreply.github.com>
2022-04-24 16:56:25 +0700

    Update main.js

--- main.js
+++ main.js
@@ -38,7 +38,7 @@ global.prefix = new RegExp('^[' + (opts['prefix'] || '‎xzXZ/i!#$%+£¢€¥^°
 global.db = new Low(
   /https?:\/\//.test(opts['db'] || '') ?
     new cloudDBAdapter(opts['db']) : /mongodb/.test(opts['db']) ?
-      new mongoDB(opts['mongod']) :
+      new mongoDB(opts['db']) :
       new JSONFile(`${opts._[0] ? opts._[0] + '_' : ''}database.json`)
 )
 global.DATABASE = global.db // Backwards Compatibility
